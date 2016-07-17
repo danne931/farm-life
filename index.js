8afc020203f9883d1b2e5acca6ee2f1e09a7fe75
Daniel Eisenbarger <danne9.31@gmail.com>
2016-07-16 17:04:19 -0700

    clarity

--- index.js
+++ index.js
@@ -13,46 +13,46 @@ const placeStrAtChar = type => (str, searchChar, placementStr) => {
   const args = [str, searchChar, placementStr]
   if (someNull(args) || someNotOfTypeStrOrNum(args)) return ''
   const placementStrSize = placementStr.length
-  let strArr = []
+  let acc = []
 
-  return (function _placeStrAtChar (str) {
-    const locFound = str.search(escapeSpecialChars(searchChar))
+  return (function _placeStrAtChar (subset) {
+    const locFound = subset.search(escapeSpecialChars(searchChar))
     if (locFound === -1) {
-      return strArr.length ? strArr.concat(str).join('') : str
+      return acc.length ? acc.concat(subset).join('') : subset
     }
 
-    const nextSubStr = str.slice(locFound + 1)
+    const nextSubset = subset.slice(locFound + 1)
     const accStrPrecedingLocFound = () =>
-      strArr.concat(str.slice(0, locFound + 1))
+      acc.concat(subset.slice(0, locFound + 1))
 
     if (type === 'prepend') {
       const locStartPotential = locFound - placementStrSize
-      const potential = str.slice(locStartPotential, locFound)
+      const potential = subset.slice(locStartPotential, locFound)
 
       if (locStartPotential >= 0 && potential === placementStr) {
-        strArr = accStrPrecedingLocFound()
-        return strArr.length ? _placeStrAtChar(nextSubStr) : str
+        acc = accStrPrecedingLocFound()
+        return subset.length ? _placeStrAtChar(nextSubset) : subset
       }
 
-      strArr = strArr
-        .concat(str.slice(0, locFound))
+      acc = acc
+        .concat(subset.slice(0, locFound))
         .concat([placementStr])
-        .concat(str.slice(locFound, locFound + 1))
+        .concat(subset.slice(locFound, locFound + 1))
     } else {
       const locEndPotential = locFound + placementStrSize
-      const potential = str.slice(locFound + 1, locEndPotential + 1)
+      const potential = subset.slice(locFound + 1, locEndPotential + 1)
 
       if (locEndPotential >= 0 && potential === placementStr) {
-        strArr = accStrPrecedingLocFound()
-        return strArr.length ? _placeStrAtChar(nextSubStr) : str
+        acc = accStrPrecedingLocFound()
+        return subset.length ? _placeStrAtChar(nextSubset) : subset
       }
 
-      strArr = strArr
-        .concat(str.slice(0, locFound + 1))
+      acc = acc
+        .concat(subset.slice(0, locFound + 1))
         .concat([placementStr])
     }
 
-    return _placeStrAtChar(nextSubStr)
+    return _placeStrAtChar(nextSubset)
   })(str)
 }
 
